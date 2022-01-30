a602cf243cd33e989eac39f7d69c5fef556ca0b1
kangmoonsun <kangmoonsun9@naver.com>
2022-01-30 14:09:40 +0900

    second commit

---
+++ january/src/january30/BaekJoon18108.java
@@ -0,0 +1,13 @@
+package january30;
+
+import java.util.Scanner;
+
+public class BaekJoon18108 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int year = scanner.nextInt();
+		
+		System.out.println(year-543);
+	}
+}
