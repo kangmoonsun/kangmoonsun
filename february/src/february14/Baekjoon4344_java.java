e9091be0842192d45680d9a584e30bf6857ba990
kangmoonsun <kangmoonsun9@naver.com>
2022-02-18 19:02:49 +0900

    20220218

---
+++ february/src/february14/Baekjoon4344_java.java
@@ -0,0 +1,24 @@
+package february14;
+
+import java.util.Scanner;
+
+public class Baekjoon4344_java {
+
+	public static void main(String[] args) {
+
+			Scanner scanner = new Scanner(System.in);
+			int a = scanner.nextInt();
+			
+			for(int i = 0 ; i < a ; i++) {
+				int b = scanner.nextInt();
+				for(int j = 0 ; j < b; j++) {
+					int arr[] = new int[b];
+					
+					double sum = 0;
+					
+					for
+					}
+			}
+	}
+
+}
---
+++ february/src/february18/Baekjoon15596_java.java
@@ -0,0 +1,16 @@
+package february18;
+
+class Baekjoon15596_java {
+	
+		long sum(int[] a) {
+			long sum = 0;
+			
+			for(int i = 0; i <a.length; i++) {
+				sum +=a[i];
+			}
+			return sum;
+		}
+	
+	}
+	
+	
